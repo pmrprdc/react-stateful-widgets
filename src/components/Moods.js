25f61115a9faae443a557bd335f09e039cadf60b
Pamir Perdeci <pamirperdeci@gmail.com>
2022-12-07 11:18:26 -0500

    color of mood changes according to state

--- src/components/Moods.js
+++ src/components/Moods.js
@@ -37,6 +37,8 @@ const sadMood = 'Rather sad';
 export default function Moods() {
   /* STEP 1 */
 
+  const [mood, setMood] = useState(initialMood);
+
   const makeHappy = () => {
     /* STEP 4 */
   };
@@ -47,20 +49,30 @@ export default function Moods() {
     /* STEP 6 */
   };
 
+  
+
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
     color: 'crimson', /* STEP 2 */
   };
 
+
+  if (mood === happyMood){
+
+    style.color = "royalblue";
+  } 
+
+
+
   return (
     <div className='widget-moods container'>
       <h2>Moods</h2>
       <div id='mood' style={style}>Not sure how I feel</div> {/* STEP 3 */}
       <div>
-        <button id='makeHappy' onClick={makeHappy}>Make Happy</button>
-        <button id='makeSad' onClick={makeSad}>Make Sad</button>
-        <button id='resetMood' onClick={reset}>Reset</button>
+        <button id='makeHappy' onClick={makeHappy}>{mood}</button>
+        <button id='makeSad' onClick={makeSad}>{mood}</button>
+        <button id='resetMood' onClick={reset}>{mood}</button>
       </div>
     </div>
   );
